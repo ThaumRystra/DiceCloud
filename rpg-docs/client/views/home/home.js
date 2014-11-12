3956fffa0c8ad2d06b218710287613b7970cdee9
Thaum <stefanzermatten@gmail.com>
2014-11-12 20:08:53 +0000

    Changed character list for individual character deletion

--- rpg-docs/client/views/home/home.js
+++ rpg-docs/client/views/home/home.js
@@ -2,10 +2,7 @@ Template.home.events({
 	"click #addCharacter": function (event, template) {
 		Characters.insert(new Character(Meteor.userId()));
 	},
-	"click #nukeCharacters": function(event, template){
-		while(true){
-			if(!Characters.findOne()) break;
-			Characters.remove(Characters.findOne()._id);
-		}
+	"click #delete": function(event, template){
+		Characters.remove(this._id);
 	}
 });
