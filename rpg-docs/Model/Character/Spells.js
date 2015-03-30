35ce49ded8b82638d83bbe6e87af29d327e13ef7
Thaum <stefanzermatten@gmail.com>
2015-03-30 09:55:52 +0000

    Improved Spell default values

--- rpg-docs/Model/Character/Spells.js
+++ rpg-docs/Model/Character/Spells.js
@@ -2,20 +2,20 @@ Spells = new Mongo.Collection("spells");
 
 Schemas.Spell = new SimpleSchema({
 	charId:      {type: String, regEx: SimpleSchema.RegEx.Id},
-	prepared:    {type: String, defaultValue: "unprepared", allowedValues: ["prepared","unprepared","always"]},
-	name:		 {type: String, trim: false},
+	prepared:    {type: String, defaultValue: "prepared", allowedValues: ["prepared","unprepared","always"]},
+	name:        {type: String, trim: false, defaultValue: "New Spell"},
 	description: {type: String, optional: true, trim: false},
 	castingTime: {type: String, optional: true, defaultValue: "action", trim: false},
-	range:		 {type: String, optional: true, trim: false},
-	duration:	 {type: String, optional: true, trim: false},
+	range:       {type: String, optional: true, trim: false},
+	duration:    {type: String, optional: true, trim: false, defaultValue: "Instantaneous"},
 	"components.verbal":		{type: Boolean, defaultValue: false},
 	"components.somatic":		{type: Boolean, defaultValue: false},
 	"components.material":		{type: String, optional: true},
 	"components.concentration":	{type: Boolean, defaultValue: false},
-	ritual:		 {type: Boolean, defaultValue: false},
-	level:		 {type: Number, defaultValue: 0},
+	ritual:      {type: Boolean, defaultValue: false},
+	level:       {type: Number, defaultValue: 1},
 	school:      {type: String, defaultValue: "Abjuration", allowedValues: magicSchools},
-	color:   {type: String, allowedValues: _.pluck(colorOptions, "key"), defaultValue: "q"}
+	color:       {type: String, allowedValues: _.pluck(colorOptions, "key"), defaultValue: "q"}
 });
 
 Spells.attachSchema(Schemas.Spell);
