d785e1ff59b5acfc4511112f477f9b18c70d5bca
Thaum <stefanzermatten@gmail.com>
2015-04-14 14:06:50 +0000

    Finished implementing basic sharing

--- rpg-docs/client/views/character/characterSettings/shareDialog.js
+++ rpg-docs/client/views/character/characterSettings/shareDialog.js
@@ -1,29 +1,55 @@
-Template.shareDialog.helpers({
+Template.shareDialog.onCreated(function(){
+	this.userId = new ReactiveVar();
+});
 
+Template.shareDialog.helpers({
+	readers: function(){
+		var char = Characters.findOne(this._id, {fields: {readers: 1}});
+		return char && char.readers;
+	},
+	writers: function(){
+		var char = Characters.findOne(this._id, {fields: {writers: 1}});
+		return char && char.writers;
+	},
+	shareButtonDisabled: function(){
+		return !Template.instance().userId.get();
+	},
+	userFindError: function(){
+		if (!Template.instance().userId.get()){
+			return "User not found";
+		}
+	}
 });
 
 Template.shareDialog.events({
-	"tap #shareButton": function(event, instance){
-        var self = this;
+	"input #userNameOrEmailInput, change #userNameOrEmailInput": function(event, instance){
 		var userName = instance.find("#userNameOrEmailInput").value;
-		var permission = instance.find("#accessLevelMenu").value;
-        Meteor.call("getUserId", userName, function (err, result) {
-            if(err){
-                this.userFindError = true;
-            } else{
-                if(permission === "write"){
-                    Characters.update(self._id, {
-                        $push: {writers: result},
-                        $pull: {readers: result}
-                    });
-                } else {
-                    Characters.update(self._id, {
-                        $push: {readers: result},
-                        $pull: {writers: result}
-                    });
-                }
-                GlobalUI.closeDialog();
-            }
-        });
+		instance.userId.set(undefined);
+		Meteor.call("getUserId", userName, function (err, result) {
+			if(err){
+				console.error(err);
+			} else{
+				console.log(result);
+				instance.userId.set(result);
+			}
+		});
+	},
+	"tap #shareButton": function(event, instance){
+		var self = this;
+		var permission = instance.find("#accessLevelMenu").selected;
+		if(!permission) throw "no permission set";
+		var userId = instance.userId.get();
+		if(!userId) return;
+		if(permission === "write"){
+			Characters.update(self._id, {
+				$addToSet: {writers: userId},
+				$pull: {readers: userId}
+			});
+		} else {
+			Characters.update(self._id, {
+				$addToSet: {readers: userId},
+				$pull: {writers: userId}
+			});
+		}
 	}
 });
---
+++ rpg-docs/lib/functions/shareCharacter.js
@@ -0,0 +1,7 @@
+Meteor.methods({
+	"getUserId": function(username){
+		if(!username) return;
+		var user = Meteor.users.findOne({$or: [{username: username}, {"emails.address": username}]});
+		return user && user._id;
+	}
+});
