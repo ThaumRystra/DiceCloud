895a099b9b2657236c33bf1f5b09d07eb1562a39
Stefan Zermatten <stefanzermatten@gmail.com>
2015-04-23 10:00:31 +0200

    Added migration methods available to admins

--- rpg-docs/server/migrations/migrations.js
+++ rpg-docs/server/migrations/migrations.js
@@ -1,3 +1,26 @@
+Meteor.methods({
+	getVersion: function() {
+		return Migrations.getVersion();
+	},
+	migrateTo: function(versionNumber) {
+		var user = Meteor.users.findOne(this.userId);
+		if (!user){
+			throw new Meteor.Error(
+				"logged-out", 
+				"The user must be logged in to migrate the database"
+			);
+		}
+		if (_.contains(user.roles, "admin")){
+			Migrations.migrateTo(versionNumber);
+		} else {
+			throw new Meteor.Error(
+				"not admin",
+				"The user must be an administrator to migrate the database"
+			);
+		}
+	}
+});
+
 Migrations.add({
 	version: 1,
 	name: "converts effect proficiencies to proficiency objects, removes types from assets",
