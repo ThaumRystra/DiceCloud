f8d2b4017f8741cb12db9fd6c4d4c9c3a661c443
Stefan Zermatten <stefanzermatten@gmail.com>
2016-03-09 18:11:46 +0200

    Created an icon selector that lets you search for icons

--- rpg-docs/client/views/meta/iconList/iconList.js
+++ rpg-docs/client/views/meta/iconList/iconList.js
@@ -1,5 +1,16 @@
+Template.iconList.onCreated(function(){
+	this.selected = new ReactiveVar();
+});
+
 Template.iconList.helpers({
-	rpgIconNames: function() {
-		return rpgIconNames;
-	}
+	selected: function(){
+		return Template.instance().selected.get();
+	},
+});
+
+Template.iconList.events({
+	"icon-selected": function(event, template){
+		var val = event.originalEvent.detail;
+		template.selected.set(val);
+	},
 });
