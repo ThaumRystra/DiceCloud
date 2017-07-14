44a1daf6f8cc13a427bb496baf3bb1efe5a22265
Stefan Zermatten <stefanzermatten@gmail.com>
2017-07-14 17:09:30 +0200

    Grouping characters by party now works
    closes #75, finally

--- rpg-docs/client/views/characterList/characterList.js
+++ rpg-docs/client/views/characterList/characterList.js
@@ -1,35 +1,57 @@
 Template.characterList.helpers({
-	characters(){
+	characters() {
 		var userId = Meteor.userId();
 		return Characters.find(
-			{
-				$or: [
-					{readers: userId},
-					{writers: userId},
-					{owner: userId},
-				]
-			},
-			{
-				fields: {
-					name: 1,
-					urlName: 1,
-					picture: 1,
-					color: 1,
-					race: 1,
-					alignment: 1,
-					gender: 1,
-				},
-				sort: {name: 1},
-			}
+			{$or: [{readers: userId}, {writers: userId}, {owner: userId}]},
+			{sort: {name: 1}}
 		);
 	},
+	parties() {
+		return Parties.find({owner: Meteor.userId()});
+	},
+	charactersInParty(partyId) {
+		var userId = Meteor.userId();
+		var party = Parties.findOne(partyId);
+		return Characters.find(
+			{
+				_id: {$in: party.characters},
+				$or: [{readers: userId}, {writers: userId}, {owner: userId}],
+			},
+			{sort: {name: 1}}
+		);
+	},
+	charactersWithNoParty() {
+		var userId = Meteor.userId();
+		var charArrays = Parties.find({owner: userId}).map(p => p.characters);
+		var partyChars = _.uniq(_.flatten(charArrays));
+		return Characters.find(
+			{
+				_id: {$nin: partyChars},
+				$or: [{readers: userId}, {writers: userId}, {owner: userId}],
+			},
+			{sort: {name: 1}}
+		);
+	},
+});
+
+Template.characterCard.helpers({
 	initials(name){
 		return name.replace(/[^A-Z]/g, "");
 	},
-})
+});
 
 Template.characterList.events({
-	"tap .addCharacter": function(event, template) {
+	"click .partyHeader": function(event, instance){
+		pushDialogStack({
+			template: "partyDialog",
+			data:     {
+				_id: this._id,
+				startEditing: true,
+			},
+			element:   event.currentTarget.parentElement,
+		});
+	},
+	"click .addCharacter": function(event, instance) {
 		pushDialogStack({
 			template: "newCharacterDialog",
 			element: event.currentTarget,
@@ -37,8 +59,23 @@ Template.characterList.events({
 				if (!character) return;
 				character.owner = Meteor.userId();
 				let _id = Characters.insert(character);
-				Router.go("characterSheet", {_id});
+				let urlName = getSlug(character.name, {maintainCase: true}) || "-"
+				Router.go("characterSheet", {_id, urlName});
 			},
 		})
+	},
+	"click .addParty": function(event, instance) {
+		var partyId = Parties.insert({
+			owner: Meteor.userId(),
+		});
+		pushDialogStack({
+			template: "partyDialog",
+			data:     {
+				_id: partyId,
+				startEditing: true,
+			},
+			element:   event.currentTarget,
+			returnElement: instance.find(`.party[data-id='${partyId}']`),
+		});
 	},
 });
--- rpg-docs/server/publications/characterList.js
+++ rpg-docs/server/publications/characterList.js
@@ -4,27 +4,24 @@ Meteor.publish("characterList", function(){
 		this.ready();
 		return;
 	}
-	return Characters.find(
-		{
-			$or: [
-				{readers: userId},
-				{writers: userId},
-				{owner: userId},
-			]
-		},
-		{
-			fields: {
-				name: 1,
-				urlName: 1,
-				race: 1,
-				alignment: 1,
-				gender: 1,
-				readers: 1,
-				writers:1,
-				owner: 1,
-				color: 1,
-				picture: 1,
+	return [
+			Characters.find(
+			{$or: [{readers: userId}, {writers: userId}, {owner: userId}]},
+			{
+				fields: {
+					name: 1,
+					urlName: 1,
+					race: 1,
+					alignment: 1,
+					gender: 1,
+					readers: 1,
+					writers:1,
+					owner: 1,
+					color: 1,
+					picture: 1,
+				}
 			}
-		}
-	);
+		),
+		Parties.find({owner: userId}),
+	];
 });
