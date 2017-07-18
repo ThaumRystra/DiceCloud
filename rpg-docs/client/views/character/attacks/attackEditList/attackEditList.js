aadc83391f3e3ef890b6806a1df75579e0f2ee38
Jacob <handsjacob@gmail.com>
2017-07-18 19:52:12 +0100

    Adding an attack to a spell now has attack bonus set by the spell list
    Default damage is also changed to 1d10 fire for spells

--- rpg-docs/client/views/character/attacks/attackEditList/attackEditList.js
+++ rpg-docs/client/views/character/attacks/attackEditList/attackEditList.js
@@ -7,12 +7,32 @@ Template.attackEditList.helpers({
 
 Template.attackEditList.events({
 	"tap #addAttackButton": function() {
+		if (typeof this.isSpell !== 'undefined' && this.isSpell) {
+			var parentSpell = Spells.findOne({"_id": this.parentId})
+			if (parentSpell && parentSpell.parent.collection == "SpellLists") {
+				var spellList = SpellLists.findOne({"_id":parentSpell.parent.id});
+				if (spellList && spellList.attackBonus) {
+					Attacks.insert({
+						charId: this.charId,
+						parent: {
+							id: this.parentId,
+							collection: this.parentCollection
+						},
+						attackBonus: spellList.attackBonus,
+						damage: "1d10",
+						damageType: "fire",
+					});
+					return;
+				}
+			}
+		}
+
 		Attacks.insert({
 			charId: this.charId,
 			parent: {
 				id: this.parentId,
 				collection: this.parentCollection
-			}
+			},
 		});
 	},
 });
