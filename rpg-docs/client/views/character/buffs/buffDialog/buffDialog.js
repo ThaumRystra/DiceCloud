cb648b4a28b1400e4b8e43fe972b4ee4905b9f68
Jacob <handsjacob@gmail.com>
2017-08-09 11:13:24 +0100

    Buffs now display who and what they were applied by.

--- rpg-docs/client/views/character/buffs/buffDialog/buffDialog.js
+++ rpg-docs/client/views/character/buffs/buffDialog/buffDialog.js
@@ -10,3 +10,39 @@ Template.buffDialog.events({
 		popDialogStack();
 	},
 });
+
+const typeDict = {
+	"Features": "feature",
+	"Items": "item",
+	"Spells": "spell",
+}; //really, we should only need these three
+
+Template.buffDetails.helpers({
+	appliedBy: function() {
+		if (this.type == "inate") {
+			return "Innate.";
+		} else {
+			var myName = Characters.findOne(this.charId).name;
+			var applierCharacter = Characters.findOne(this.appliedBy) || {name: "???"}
+			// "???" indicates that either we do not have read access to the buff-giver, or that the buff-giver does not exist.
+
+			if (applierCharacter.name === myName) {
+				var charName = "your "
+			} else {
+				if (applierCharacter.charName[applierCharacter.charName.length -1] === 's') {
+					var charName = applierCharacter.charName + "' ";
+				} else {
+					var charName = applierCharacter.charName + "'s ";
+				}
+			}
+
+			var type = typeDict[this.appliedByDetails.collection] || "";
+			var applierThing = global[this.appliedByDetails.collection]
+							&& global[this.appliedByDetails.collection].findOne(this.appliedByDetails.id)
+							&& global[this.appliedByDetails.collection].findOne(this.appliedByDetails.id).name
+							|| "unknown ability";
+
+			return "Applied by " + charName + type + applierThing + ".";
+		}
+	},
+});
