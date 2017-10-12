b308595dac3298e18aa8f5c16f3ac45afc93df4a
Stefan Zermatten <stefanzermatten@gmail.com>
2017-10-12 16:24:12 +0200

    Implement both tokens and rate limiting to API
    Closes #141, but still needs better UI on failure

--- rpg-docs/server/publications/user.js
+++ rpg-docs/server/publications/user.js
@@ -1,3 +1,8 @@
 Meteor.publish("user", function(){
-	return Meteor.users.find(this.userId, {fields: {roles: 1}});
+	return Meteor.users.find(this.userId, {fields: {
+		roles: 1,
+		username: 1,
+		profile: 1,
+		apiKey: 1,
+	}});
 });
