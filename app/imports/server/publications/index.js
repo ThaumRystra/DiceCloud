a5e7bd95c738141d393007cd9bae4f2b10e44e3b
Stefan Zermatten <stefanzermatten@gmail.com>
2018-11-15 11:23:57 +0200

    Indexed publications and imported them when the server starts

---
+++ app/imports/server/publications/index.js
@@ -0,0 +1,5 @@
+import "characterList.js";
+import "library.js";
+import "singleCharacter.js";
+import "user.js";
+import "users.js";
---
+++ app/server/main.js
@@ -0,0 +1,1 @@
+import "/imports/server/publications/index.js";
