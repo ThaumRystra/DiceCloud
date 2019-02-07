2f04d9ec1c52dd89404aad2e479a6b3a06430ab2
Andrew Zhu <me@andrew-zhu.com>
2019-02-07 15:45:45 -0800

    remove server check overrides

--- app/lib/constants/characterAssetAllowDeny.js
+++ app/lib/constants/characterAssetAllowDeny.js
@@ -12,20 +12,19 @@ Meteor.methods({
 
 CHARACTER_SUBSCHEMA_ALLOW = {
 	// the user must be logged in, and the user must be a writer of the character
-	// or we must be the server
 	insert: function(userId, doc) {
 		var char = Characters.findOne(
 			doc.charId,
 			{fields: {owner: 1, writers: 1}}
 		);
-		return (userId && char.owner === userId || _.contains(char.writers, userId) || Meteor.isServer);
+		return (userId && char.owner === userId || _.contains(char.writers, userId));
 	},
 	update: function(userId, doc, fields, modifier) {
 		var char = Characters.findOne(
 			doc.charId,
 			{fields: {owner: 1, writers: 1}}
 		);
-		return (userId && char.owner === userId || _.contains(char.writers, userId) || Meteor.isServer);
+		return (userId && char.owner === userId || _.contains(char.writers, userId));
 	},
 	remove: function(userId, doc) {
 		var char = Characters.findOne(
@@ -33,7 +32,7 @@ CHARACTER_SUBSCHEMA_ALLOW = {
 			{fields: {owner: 1, writers: 1}}
 		);
 		if (!char) return true;
-		return userId && char.owner === userId || _.contains(char.writers, userId) || Meteor.isServer;
+		return userId && char.owner === userId || _.contains(char.writers, userId);
 	},
 	fetch: ["charId"],
 };
