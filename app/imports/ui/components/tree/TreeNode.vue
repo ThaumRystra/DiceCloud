05867c61dd02e4e684d3eb7cef008ae685f63998
Stefan Zermatten <stefanzermatten@gmail.com>
2019-04-12 13:37:47 +0200

    Started on tree view for real

---
+++ app/imports/ui/components/tree/TreeNode.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+  <div class="layout row">
+  	<v-btn @click="expand">
+  		<v-icon>arrow-right</v-icon>
+  	</v-btn>
+  </div>
+</template>
+
+<script>
+/**
+* TreeNode's are list item views of character properties. Every property which
+* can belong to the character is shown in the tree view of the character
+* the tree view shows off the full character structure, and where each part of
+* character comes from.
+**/
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
