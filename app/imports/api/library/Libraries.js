062e5546290f6f904de4f76ed4169137eca55871
Stefan Zermatten <stefanzermatten@gmail.com>
2019-04-15 12:39:42 +0200

    Added comments to libraries

--- app/imports/api/library/Libraries.js
+++ app/imports/api/library/Libraries.js
@@ -1,6 +1,15 @@
 import schema from '/imports/api/schema.js';
 import SharingSchema from '/imports/api/sharing/SharingSchema.js';
 
+/**
+ * Libraries are trees of library nodes where each node represents a character
+ * property.
+ *
+ * Libraries can be shared, have multiple readers and writers, and can be
+ * subscribed to.
+ *
+ * Permissions to library nodes are controlled by the libraries they belong to.
+ */
 let Libraries = new Mongo.Collection('libraries');
 
 let LibrarySchema = schema({
