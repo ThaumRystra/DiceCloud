48fe0d3608e5a664e2e4e3e68ebee39ce47edaa6
Stefan Zermatten <stefanzermatten@gmail.com>
2019-05-31 15:36:19 +0200

    Started on library page UI

---
+++ app/imports/ui/library/LibraryContentsContainer.vue
@@ -0,0 +1,11 @@
+<template lang="html">
+  <tree-node-list :children="libraryChildren" :group="library._id"s/>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
---
+++ app/imports/ui/pages/Library.vue
@@ -0,0 +1,29 @@
+<template lang="html">
+	<toolbar-layout>
+		<span slot="toolbar">Library</span>
+		<v-card class="ma-4">
+			<library-contents-container>
+			<v-card-text v-else-if="$subReady.library">
+				You aren't subscribed to any libraries :O
+			</v-card-text>
+			<v-card-text v-if="!$subReady.library" class="layout row justify-center">
+				<v-progress-circular indeterminate/>
+			</v-card-text>
+		</v-card>
+		<v-btn fixed fab bottom right
+			color="primary"
+			@click="insertLibrary"
+			data-id="insert-library-fab"
+		>
+			<v-icon>add</v-icon>
+		</v-btn>
+	</toolbar-layout>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style lang="css" scoped>
+</style>
