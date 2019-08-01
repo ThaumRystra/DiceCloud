9f7d6b8ae7952d78dbb3883638e6f660f550392f
Stefan Zermatten <stefanzermatten@gmail.com>
2019-08-01 12:03:40 +0200

    began work on generalized property viewer

---
+++ app/imports/ui/properties/PropertyViewer.vue
@@ -0,0 +1,21 @@
+<template lang="html">
+	<component
+		v-if="model"
+		class="property-viewer"
+		:is="model.type"
+		:model="model"
+	/>
+</template>
+
+<script>
+export default {
+	props: {
+		model: {
+			type: Object,
+		},
+	},
+}
+</script>
+
+<style lang="css" scoped>
+</style>
