4fd62d17bd7db59683721b5167b16b669536966c
Thaum Rystra <stefanzermatten@gmail.com>
2020-04-29 11:03:47 +0200

    Updated markdown santizing because the old implementation was deprecated

--- app/imports/ui/components/MarkdownText.vue
+++ app/imports/ui/components/MarkdownText.vue
@@ -1,22 +1,27 @@
 <template lang="html">
-  <div v-html="compiledMarkdown">
-	</div>
+  <!-- eslint-disable-next-line vue/no-v-html -->
+  <div v-html="compiledMarkdown" />
 </template>
 
 <script>
 	import marked from 'marked';
+  import DOMPurify from 'dompurify';
 
 	export default {
 		props: {
-			markdown: String,
+			markdown: {
+        type: String,
+        default: undefined,
+      },
 		},
 		computed: {
-	    compiledMarkdown() {
+      compiledMarkdown() {
 				if (!this.markdown) return;
-				//TODO sanitizing by marked is deprecated
 				//TODO Markdown <hr> need to be styled to match their vue components
-				return marked(this.markdown, { sanitize: true });
-	    },
-	  },
+        let html = marked(this.markdown);
+        let cleanHtml = DOMPurify.sanitize(html);
+				return cleanHtml;
+      },
+    },
 	}
 </script>
