6c8e9037e19cbc6968cae7ae3370385d3a7348f9
Thaum Rystra <stefanzermatten@gmail.com>
2020-05-08 16:20:00 +0200

    Fixed function being replaced with object['value'] accessors

--- app/imports/api/creature/computation/utility/replaceBareSymbolsWithValueAccessor.js
+++ app/imports/api/creature/computation/utility/replaceBareSymbolsWithValueAccessor.js
@@ -1,7 +1,17 @@
 import * as math from 'mathjs';
 
 export default function replaceBareSymbolsWithValueAccessor(node, path) {
-  if (node.isSymbolNode && path !== 'object') {
+  if (node.isFunctionNode){
+    let fn = node.fn;
+    if (fn && fn.isSymbolNode){
+      fn.skipReplacement = true;
+    }
+    return node;
+  } else if (
+    node.isSymbolNode &&
+    path !== 'object' &&
+    node.skipReplacement !== true
+  ) {
     const object = new math.SymbolNode(node.name);
     const address = new math.ConstantNode('value');
     const index = new math.IndexNode([address]);
