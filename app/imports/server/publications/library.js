30ab216dc1cff55603c534cde1e0f640d06aaf12
Thaum Rystra <stefanzermatten@gmail.com>
2020-05-09 13:45:11 +0200

    removed redundant publications

--- app/imports/server/publications/library.js
+++ app/imports/server/publications/library.js
@@ -9,24 +9,6 @@ Meteor.publish('standardLibraries', function(){
 	return Libraries.find({_id: {$in: standardLibraryIds}});
 });
 
-Meteor.publish('standardLibraryItems', function(categoryKey){
-	return LibraryItems.find({
-		library: {$in: standardLibraryIds},
-		'settings.category': categoryKey,
-	}, {
-		sort: {name: 1},
-	});
-});
-
-Meteor.publish('standardLibrarySpells', function(level){
-	return LibrarySpells.find({
-		library: {$in: standardLibraryIds},
-		level,
-	}, {
-		sort: {name: 1},
-	});
-});
-
 Meteor.publish('libraries', function(){
 	const user = Meteor.user();
 	const userId = user && user._id;
