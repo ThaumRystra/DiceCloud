cd413ba64f35b92453df6f183cff297c2159d18a
Thaum Rystra <stefanzermatten@gmail.com>
2020-05-28 20:17:16 +0200

    Added icon for set effects

--- app/imports/ui/utility/getEffectIcon.js
+++ app/imports/ui/utility/getEffectIcon.js
@@ -5,10 +5,11 @@ export default function getEffectIcon(op, value){
     case 'mul': return 'clear';
     case 'min': return 'unfold_more';
     case 'max': return 'unfold_less';
+    case 'set': return 'push_pin';
     case 'advantage': return 'arrow_upward';
     case 'disadvantage': return 'arrow_downward';
     case 'passiveAdd': return value < 0 ? 'remove_circle_outline' : 'add_circle_outline';
     case 'fail': return 'block';
     case 'conditional': return '*' ;
   }
-};
+}
