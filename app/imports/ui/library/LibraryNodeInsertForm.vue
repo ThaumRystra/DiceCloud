fb2f1efa7282f4637d943f66ec43bccaecdd6036
Stefan Zermatten <stefanzermatten@gmail.com>
2020-05-31 19:00:32 +0200

    Property insert forms now have color selectors

--- app/imports/ui/library/LibraryNodeInsertForm.vue
+++ app/imports/ui/library/LibraryNodeInsertForm.vue
@@ -1,8 +1,18 @@
 <template lang="html">
-  <dialog-base :override-back-button="() => $emit('back')">
-    <v-toolbar-title slot="toolbar">
-      Add {{ propertyName }}
-    </v-toolbar-title>
+  <dialog-base
+    :override-back-button="() => $emit('back')"
+    :color="model.color"
+  >
+    <template slot="toolbar">
+      <v-toolbar-title>
+        Add {{ propertyName }}
+      </v-toolbar-title>
+      <v-spacer />
+      <color-picker
+        :value="model.color"
+        @input="value => change({path: ['color'], value})"
+      />
+    </template>
     <component
       :is="type"
       v-if="type"
@@ -32,12 +42,14 @@
 import DialogBase from '/imports/ui/dialogStack/DialogBase.vue';
 import propertyFormIndex from '/imports/ui/properties/forms/shared/propertyFormIndex.js';
 import schemaFormMixin from '/imports/ui/properties/forms/shared/schemaFormMixin.js';
+import ColorPicker from '/imports/ui/components/ColorPicker.vue';
 import propertySchemasIndex from '/imports/api/properties/propertySchemasIndex.js';
 
 export default {
 	components: {
 		...propertyFormIndex,
 		DialogBase,
+    ColorPicker,
 	},
 	mixins: [schemaFormMixin],
 	props: {
