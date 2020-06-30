308168791b0e2ed84bfc1a784b580b9a33b12681
Stefan Zermatten <stefanzermatten@gmail.com>
2020-06-30 15:15:55 +0200

    Made dX rolls work as 1dX

--- app/imports/api/creature/computation/afterComputation/substituteRollsWithFunctions.js
+++ app/imports/api/creature/computation/afterComputation/substituteRollsWithFunctions.js
@@ -14,6 +14,14 @@ export default function substituteRollsWithFunctions(node){
     let diceSize = node.args[1].name.slice(1);
     let diceSizeNode = new math.ConstantNode(diceSize);
     return new math.FunctionNode('roll', [node.args[0], diceSizeNode]);
+  } else if (
+    node.isSymbolNode &&
+    diceRegex.test(node.name)
+  ) {
+    let diceSize = node.name.slice(1);
+    let diceSizeNode = new math.ConstantNode(diceSize);
+    let diceNumberNode = new math.ConstantNode(1);
+    return new math.FunctionNode('roll', [diceNumberNode, diceSizeNode]);
   } else {
     return node;
   }
