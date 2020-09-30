844588cdbfe51c105c9ad5ec6db19b5c1c1403ab
Stefan Zermatten <stefanzermatten@gmail.com>
2020-09-30 16:24:33 +0200

    Started adding text input to log tab

--- app/imports/ui/creature/character/characterSheetTabs/LogTab.vue
+++ app/imports/ui/creature/character/characterSheetTabs/LogTab.vue
@@ -1,22 +1,74 @@
 <template lang="html">
-  <div class="log layout column reverse">
-    <div
-      v-for="log in logs"
-      :key="log._id"
-    >
-      {{ log.text }}
+  <div
+    style="min-height: calc(100vh - 96px);"
+    class="layout column justify-end"
+  >
+    <div class="log flex layout column reverse">
+      <div
+        v-for="log in logs"
+        :key="log._id"
+      >
+        {{ log.text }}
+      </div>
     </div>
+    <v-card>
+      <v-text-field
+        v-model="input"
+        solo
+        persistent-hint
+        style="flex-grow: 0"
+        :hint="inputHint"
+        :error="inputError"
+        :disabled="!editPermission"
+        @input="inputChanged"
+      />
+    </v-card>
   </div>
 </template>
 
 <script>
 import CreatureLogs from '/imports/api/creature/log/CreatureLogs.js';
+import Creatures from '/imports/api/creature/Creatures.js';
+import { assertEditPermission } from '/imports/api/creature/creaturePermissions.js';
+import { parse } from '/imports/parser/parser.js';
 
 export default {
   props: {
     creatureId: {
       type: String,
       required: true,
+    },
+  },
+  data(){return {
+    inputHint: undefined,
+    inputError: undefined,
+    input: undefined,
+  }},
+  watch: {
+    input(value){
+      this.input = value;
+      this.inputHint = this.inputError = undefined;
+      let result;
+      try {
+        result = parse(value);
+      } catch (e){
+        console.error(e);
+        this.inputError = 'Invalid syntax';
+        return;
+      }
+      if (result === null){
+        this.inputError =  '...';
+        return;
+      }
+      try {
+        let compiled = result.compile(this.creature.variables);
+        this.inputHint = compiled.toString();
+        return;
+      } catch (e){
+        console.error(e);
+        this.inputError = 'Compilation error';
+        return;
+      }
     },
   },
   meteor: {
@@ -27,6 +79,17 @@ export default {
         limit: 20,
         sort: {date: -1},
       });
+    },
+    creature(){
+      return Creatures.findOne(this.creatureId) || {};
+    },
+    editPermission(){
+      try {
+        assertEditPermission(this.creature, Meteor.userId());
+        return true;
+      } catch (e) {
+        return false;
+      }
     },
   },
 }
