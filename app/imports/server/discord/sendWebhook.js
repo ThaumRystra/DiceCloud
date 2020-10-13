0af0afc0d03a63ffbff53dbb4d545d2b6904f671
Stefan Zermatten <stefanzermatten@gmail.com>
2020-10-13 12:42:02 +0200

    Discord webhooks now mirror character log

--- app/imports/server/discord/sendWebhook.js
+++ app/imports/server/discord/sendWebhook.js
@@ -1,24 +1,23 @@
 import Discord from 'discord.js'
-export default function sendWebhook({webhookURL, message}){
+export default function sendWebhook({webhookURL, message, options}){
   //webhookURL = https://discordapp.com/api/webhooks/<id>/<token>
   let urlArray = webhookURL.split('/');
   let token = urlArray.pop();
   let id = urlArray.pop();
-  // const hook = new Discord.WebhookClient(webhook.id, webhook.token);
   const hook = new Discord.WebhookClient(id, token);
   // Send a message using the webhook
-  hook.send(message);
+  hook.send(message, options)
 }
 
 export function sendWebhookAsCreature({creature, content, embeds}){
-  if (!creature || !creature.discordWebhook) return;
+  if (!creature || !creature.settings || !creature.settings.discordWebhook) return;
   sendWebhook({
-    webhookURL: creature.discordWebhook,
-    message: {
+    webhookURL: creature.settings.discordWebhook,
+    message: content,
+    options: {
       username: creature.name,
-      avatar_url: creature.avatarPicture,
-      content,
+      avatarURL: creature.avatarPicture,
       embeds,
-    }
+    },
   });
 }
