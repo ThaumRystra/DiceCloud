e00dfe1532437316bfeeff4e8f4a05bf599a3666
Stefan Zermatten <stefanzermatten@gmail.com>
2021-03-02 14:31:35 +0200

    Changed the color of the log background

--- app/imports/ui/creature/character/CharacterSheetRightDrawer.vue
+++ app/imports/ui/creature/character/CharacterSheetRightDrawer.vue
@@ -5,15 +5,15 @@
     right
     clipped
   >
-    <log-tab :creature-id="$route.params.id" />
+    <character-log :creature-id="$route.params.id" />
   </v-navigation-drawer>
 </template>
 
 <script>
-import LogTab from '/imports/ui/log/CharacterLog.vue';
+import CharacterLog from '/imports/ui/log/CharacterLog.vue';
 export default {
   components: {
-    LogTab,
+    CharacterLog,
   },
   computed: {
     drawer: {
