109b89022e18fc65d850930017bcd82a05800319
Stefan Zermatten <stefanzermatten@gmail.com>
2021-04-11 13:09:16 +0200

    removed id from comment

--- app/imports/api/creature/v1Migration/migrateCharacter.js
+++ app/imports/api/creature/v1Migration/migrateCharacter.js
@@ -1,8 +1,8 @@
 import { fetch } from 'meteor/fetch'
 
 export default function importCharacter(url){
-  // Using v1's JSON API to fetch the character data in a nice format
-  // url -> https://dicecloud.com/character/HxerC2ZYXKrNc8u2M/json?key=<key>
+  // Using v1's JSON API to fetch the character data in a usable format
+  // url -> https://dicecloud.com/character/<id>/json?key=<key>
   fetch(url)
     .then(response => response.json())
     .then(data => {
