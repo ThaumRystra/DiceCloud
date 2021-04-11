0c2423806956c9b40fdb162bdde52229346637cc
Stefan Zermatten <stefanzermatten@gmail.com>
2021-04-11 18:15:56 +0200

    Fixed not found page for Vuetify 2

--- app/imports/ui/pages/NotFound.vue
+++ app/imports/ui/pages/NotFound.vue
@@ -1,12 +1,19 @@
 <template>
-  <div>
-    <v-layout
-      align-center
-      justify-center
-    >
-      <h1>
-        No page was found for this address
-      </h1>
-    </v-layout>
-  </div>
+  <v-layout
+    style="height: 100%;"
+    column
+    align-center
+    justify-center
+  >
+    <h1 class="text-h1">
+      404
+    </h1>
+    <h1 class="ma-4 text-h3">
+      No page was found for this address
+    </h1>
+  </v-layout>
 </template>
+
+<script lang="js">
+  export default {}
+</script>
