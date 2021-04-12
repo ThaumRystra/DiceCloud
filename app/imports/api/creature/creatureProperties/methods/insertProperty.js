e30754ef261602ea5e22893b6a91c51f50716f52
Stefan Zermatten <stefanzermatten@gmail.com>
2021-04-12 15:35:25 +0200

    Added method to insert property to a tagged parent

--- app/imports/api/creature/creatureProperties/methods/insertProperty.js
+++ app/imports/api/creature/creatureProperties/methods/insertProperty.js
@@ -2,16 +2,25 @@ import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import { RateLimiterMixin } from 'ddp-rate-limiter-mixin';
 import CreatureProperties from '/imports/api/creature/creatureProperties/CreatureProperties.js';
 import getRootCreatureAncestor from '/imports/api/creature/creatureProperties/getRootCreatureAncestor.js';
+import SimpleSchema from 'simpl-schema';
 import { assertEditPermission } from '/imports/api/sharing/sharingPermissions.js';
 import { reorderDocs } from '/imports/api/parenting/order.js';
 import recomputeInactiveProperties from '/imports/api/creature/denormalise/recomputeInactiveProperties.js';
 import { recomputeCreatureByDoc } from '/imports/api/creature/computation/methods/recomputeCreature.js';
 import recomputeInventory from '/imports/api/creature/denormalise/recomputeInventory.js';
 import { getAncestry } from '/imports/api/parenting/parenting.js';
+import getParentRefByTag from '/imports/api/creature/creatureProperties/methods/getParentRefByTag.js';
+import { RefSchema } from '/imports/api/parenting/ChildSchema.js';
 
 const insertProperty = new ValidatedMethod({
   name: 'creatureProperties.insert',
-	validate: null,
+	validate: new SimpleSchema({
+    creatureProperty: {
+      type: Object,
+      blackbox: true,
+    },
+    parentRef: RefSchema,
+  }).validator(),
   mixins: [RateLimiterMixin],
   rateLimit: {
     numRequests: 5,
@@ -31,6 +40,75 @@ const insertProperty = new ValidatedMethod({
 			throw `${parentRef.collection} is not a valid parent collection`
 		}
     assertEditPermission(rootCreature, this.userId);
+
+    creatureProperty.parent = parentRef;
+    creatureProperty.ancestors = ancestors;
+
+		return insertPropertyWork({
+      property: creatureProperty,
+      creature: rootCreature,
+    });
+  },
+});
+
+const insertPropertyAsChildOfTag = new ValidatedMethod({
+  name: 'creatureProperties.insertAsChildOfTag',
+  validate: new SimpleSchema({
+    creatureProperty: {
+      type: Object,
+      blackbox: true,
+    },
+    creatureId: {
+      type: String,
+      regEx: SimpleSchema.RegEx.Id,
+    },
+    tag: {
+      type: String,
+      max: 20,
+    },
+  }).validator(),
+  mixins: [RateLimiterMixin],
+  rateLimit: {
+    numRequests: 5,
+    timeInterval: 5000,
+  },
+  run({creatureProperty, creatureId, tag}) {
+    let parentRef = getParentRefByTag(creatureId, tag);
+
+    if (!parentRef){
+      // Use the creature as the parent and mark that we need to insert the folder first later
+      var insertFolderFirst = true;
+      parentRef = {id: creatureId, collection: 'creatures'};
+    }
+
+    // get the new ancestry for the properties
+		let {parentDoc, ancestors} = getAncestry({parentRef});
+
+    // Check permission to edit
+    let rootCreature;
+		if (parentRef.collection === 'creatures'){
+      rootCreature = parentDoc;
+		} else if (parentRef.collection === 'creatureProperties'){
+      rootCreature = getRootCreatureAncestor(parentDoc);
+		} else {
+			throw `${parentRef.collection} is not a valid parent collection`
+		}
+    assertEditPermission(rootCreature, this.userId);
+
+    // Add the folder first if we need to
+    if (insertFolderFirst){
+      let id = CreatureProperties.insert({
+        type: 'folder',
+        name: tag.charAt(0).toUpperCase() + tag.slice(1),
+        tags: [tag],
+        parent: parentRef,
+        ancestors: [parentRef],
+      });
+      // Make the folder our new parent
+      let newParentRef = {id, collection: 'creatureProperties'};
+      ancestors = [parentRef, newParentRef];
+      parentRef = newParentRef;
+    }
 
     creatureProperty.parent = parentRef;
     creatureProperty.ancestors = ancestors;
@@ -63,3 +141,4 @@ export function insertPropertyWork({property, creature}){
 }
 
 export default insertProperty;
+export { insertPropertyAsChildOfTag };
