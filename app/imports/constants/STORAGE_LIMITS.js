9f62a78eb0a7a30c9d83f2478599c43b9df7ae65
Stefan Zermatten <stefanzermatten@gmail.com>
2021-08-09 22:34:04 +0200

    Began work on implementing string and array storage limits

---
+++ app/imports/constants/STORAGE_LIMITS.js
@@ -0,0 +1,14 @@
+const STORAGE_LIMITS = Object.freeze({
+  name: 140,
+  color: 10000,
+  summary: 10000,
+  description: 49473, //the length of the Bee Movie script
+  inlineCalculationCount: 32,
+  errorCount: 32,
+  tagCount: 64,
+  tagLength: 140,
+  resourcesCount: 32,
+  calculation: 280,
+});
+
+export default STORAGE_LIMITS;
