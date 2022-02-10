3948d20f4678d4f09aaa00056a0de80dbdb39ea1
Stefan Zermatten <stefanzermatten@gmail.com>
2022-02-10 10:37:23 +0200

    Allowed cross origin requests to REST API

--- app/imports/server/config/SimpleRestConfig.js
+++ app/imports/server/config/SimpleRestConfig.js
@@ -1,4 +1,17 @@
+import { JsonRoutes } from 'meteor/simple:json-routes';
 import { SimpleRest } from 'meteor/simple:rest';
+
+Meteor.startup(() => {
+  //
+  // Enable cross origin requests for all endpoints
+  JsonRoutes.setResponseHeaders({
+    'Cache-Control': 'no-store',
+    Pragma: 'no-cache',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, PUT, POST, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization, X-Requested-With',
+  });
+});
 
 SimpleRest.configure({
   // No default collection methods get end points
