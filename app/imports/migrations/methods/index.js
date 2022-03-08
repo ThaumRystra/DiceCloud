c68667be9c33d28412dca7a406592fa8639a8e32
Stefan Zermatten <stefanzermatten@gmail.com>
2022-03-08 15:04:51 +0200

    Added data validation diagnostics for offline use

--- app/imports/migrations/methods/index.js
+++ app/imports/migrations/methods/index.js
@@ -1,2 +1,3 @@
 import './migrateTo.js';
+import './validateDatabase.js';
 import './getVersion.js';
---
+++ app/imports/migrations/methods/validateDatabase.js
@@ -0,0 +1,35 @@
+import { ValidatedMethod } from 'meteor/mdg:validated-method';
+import { RateLimiterMixin } from 'ddp-rate-limiter-mixin';
+import { assertAdmin } from '/imports/api/sharing/sharingPermissions.js';
+
+const validateDatabase = new ValidatedMethod({
+  name: 'validateDatabase',
+	validate: null,
+  mixins: [RateLimiterMixin],
+  rateLimit: {
+    numRequests: 1,
+    timeInterval: 10000,
+  },
+  run() {
+    assertAdmin(this.userId);
+    // Very computationally expensive data diagnostics
+    // Only run in an offline instance you control
+    return;
+    if (Meteor.isClient) return;
+
+    Meteor.Collection.getAll().forEach(collection => {
+      if (!collection.instance._c2?._simpleSchemas) return;
+      collection.instance.find({}).forEach(doc => {
+        const schema = collection.instance.simpleSchema(doc);
+        let cleanDoc = schema.clean(doc);
+        try {
+          schema.validate(cleanDoc, {modifier: false});
+        } catch (e){
+          console.log(collection.name, doc._id, e.message || e.reason || e.toString());
+        }
+      });
+    });
+  },
+});
+
+export default validateDatabase;
