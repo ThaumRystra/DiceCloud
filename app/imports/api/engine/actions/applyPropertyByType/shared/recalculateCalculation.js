43f8df09f0959a45c5b22d592299ab4e7b47f73b
Stefan Zermatten <stefanzermatten@gmail.com>
2022-08-26 09:42:34 +0200

    Fixed client crash when effects target calcs

--- app/imports/api/engine/actions/applyPropertyByType/shared/recalculateCalculation.js
+++ app/imports/api/engine/actions/applyPropertyByType/shared/recalculateCalculation.js
@@ -5,7 +5,7 @@ import logErrors from './logErrors.js';
 export default function recalculateCalculation(calc, actionContext, context){
   if (!calc?.parseNode) return;
   calc._parseLevel = 'reduce';
-  applyEffectsToCalculationParseNode(calc, actionContext.log);
+  applyEffectsToCalculationParseNode(calc, actionContext);
   evaluateCalculation(calc, actionContext.scope, context);
-  logErrors(calc.errors, actionContext.log);
+  logErrors(calc.errors, actionContext);
 }
