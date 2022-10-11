a5b4b2032431b478bc5c96b6518615b1fe7e1971
Stefan Zermatten <stefanzermatten@gmail.com>
2022-10-11 14:08:33 +0200

    Added link to V2

--- app/client/views/layout/layout.js
+++ app/client/views/layout/layout.js
@@ -1,53 +1,53 @@
-Template.layout.onCreated(function() {
+Template.layout.onCreated(function () {
 	this.subscribe("user");
 });
 
 Template.layout.helpers({
-  connectionStatus: function(){
-    let status = Meteor.status()
-    return status.reason || status.status;
-  },
-  disconnected: function(){
-    return !Meteor.status().connected;
-  },
+	connectionStatus: function () {
+		let status = Meteor.status()
+		return status.reason || status.status;
+	},
+	disconnected: function () {
+		return !Meteor.status().connected;
+	},
 });
 
 Template.appDrawer.helpers({
-	profileLink: function() {
+	profileLink: function () {
 		var user = Meteor.user();
 		return user.profile && user.profile.username || user.username || "My Account";
 	},
-	showPatreonBadge: function(){
-		let post = PatreonPosts.findOne({}, {sort: {date: -1}});
+	showPatreonBadge: function () {
+		let post = PatreonPosts.findOne({}, { sort: { date: -1 } });
 		let user = Meteor.user();
 		if (!post || !user) return false;
 		return post.link !== user.lastPatreonPostClicked;
 	},
-	patreonLink: function(){
-		let post = PatreonPosts.findOne({}, {sort: {date: -1}});
+	patreonLink: function () {
+		let post = PatreonPosts.findOne({}, { sort: { date: -1 } });
 		return (post && post.link) || 'https://www.patreon.com/dicecloud';
 	},
 });
 
 let drawerLayout;
-const closeDrawer = function(instance){
+const closeDrawer = function (instance) {
 	if (!drawerLayout) drawerLayout = $("app-drawer-layout")[0];
-	if (drawerLayout && drawerLayout.narrow){
+	if (drawerLayout && drawerLayout.narrow) {
 		drawerLayout.drawer.close();
 	}
 }
 
 Template.appDrawer.events({
-	"click a": function(event, instance){
+	"click a": function (event, instance) {
 		closeDrawer(instance);
 	},
-	"click .feedback": function(event, instance) {
+	"click .feedback": function (event, instance) {
 		pushDialogStack({
 			template: "feedback",
 			element: event.currentTarget,
-			callback: function(report){
+			callback: function (report) {
 				if (!report) return;
-				Meteor.call("insertReport", report, function(e, result){
+				Meteor.call("insertReport", report, function (e, result) {
 					GlobalUI.toast({
 						text: e && e.details || "Feedback submitted"
 					});
@@ -56,13 +56,17 @@ Template.appDrawer.events({
 		});
 		closeDrawer(instance);
 	},
-	"click .patreon": function(event, instance){
-		let post = PatreonPosts.findOne({}, {sort: {date: -1}});
+	"click .patreon": function (event, instance) {
+		let post = PatreonPosts.findOne({}, { sort: { date: -1 } });
 		let link = (post && post.link) || 'https://www.patreon.com/dicecloud';
 		Meteor.call('clickPatreonPost', link);
 		ga("send", "event", "externalLink", "patreon");
 	},
-	"click .github": function(event, instance){
+	"click .github": function (event, instance) {
 		ga("send", "event", "externalLink", "github");
+	},
+	"click .version-2": function (event, instance) {
+		ga("send", "event", "externalLink", "version-2");
+		window.location = 'https://beta.dicecloud.com';
 	},
 });
