358ae4662710c1bededfd714ae1b0b3368812573
Stefan Zermatten <stefanzermatten@gmail.com>
2022-11-03 19:08:44 +0200

    Began work on copy to for library nodes

--- app/imports/api/library/methods/copyLibraryNodeTo.js
+++ app/imports/api/library/methods/copyLibraryNodeTo.js
@@ -1,6 +1,8 @@
 import { ValidatedMethod } from 'meteor/mdg:validated-method';
 import SimpleSchema from 'simpl-schema';
+i
 import { RateLimiterMixin } from 'ddp-rate-limiter-mixin';
+import { RefSchema } from '/imports/api/parenting/ChildSchema.js';
 import LibraryNodes from '/imports/api/library/LibraryNodes.js';
 import { assertDocEditPermission } from '/imports/api/sharing/sharingPermissions.js';
 import {
@@ -8,6 +10,7 @@ import {
   renewDocIds
 } from '/imports/api/parenting/parenting.js';
 import { reorderDocs } from '/imports/api/parenting/order.js';
+import fetchDocByRef from '/imports/api/parenting/fetchDocByRef.js';
 
 var snackbar;
 if (Meteor.isClient) {
@@ -18,28 +21,37 @@ if (Meteor.isClient) {
 
 const DUPLICATE_CHILDREN_LIMIT = 100;
 
-const duplicateLibraryNode = new ValidatedMethod({
-  name: 'libraryNodes.duplicate',
+const copyLibraryNodeTo = new ValidatedMethod({
+  name: 'libraryNodes.copyTo',
   validate: new SimpleSchema({
     _id: {
       type: String,
       regEx: SimpleSchema.RegEx.Id,
-    }
+    },
+    parent: {
+      type: RefSchema,
+    },
   }).validator(),
   mixins: [RateLimiterMixin],
   rateLimit: {
-    numRequests: 5,
+    numRequests: 1,
     timeInterval: 5000,
   },
-  run({ _id }) {
-    let libraryNode = LibraryNodes.findOne(_id);
+  run({ _id, parent }) {
+    if (parent.collection !== 'libraryNodes' && parent.collection !== 'libraries') {
+      throw new Meteor.Error('Invalid destination',
+        'Library documents can only be copied to destinations inside other libraries'
+      );
+    }
+    const libraryNode = LibraryNodes.findOne(_id);
+    const parentDoc = fetchDocByRef(parent);
     assertDocEditPermission(libraryNode, this.userId);
 
-    let randomSrc = DDP.randomStream('duplicateLibraryNode');
+    let randomSrc = DDP.randomStream('copyLibraryNodeTo');
     let libraryNodeId = randomSrc.id();
     libraryNode._id = libraryNodeId;
 
-    let nodes = LibraryNodes.find({
+    let decendants = LibraryNodes.find({
       'ancestors.id': _id,
       removed: { $ne: true },
     }, {
@@ -47,8 +59,8 @@ const duplicateLibraryNode = new ValidatedMethod({
       sort: { order: 1 },
     }).fetch();
 
-    if (nodes.length > DUPLICATE_CHILDREN_LIMIT) {
-      nodes.pop();
+    if (decendants.length > DUPLICATE_CHILDREN_LIMIT) {
+      decendants.pop();
       if (Meteor.isClient) {
         snackbar({
           text: `Only the first ${DUPLICATE_CHILDREN_LIMIT} children were duplicated`,
@@ -56,28 +68,29 @@ const duplicateLibraryNode = new ValidatedMethod({
       }
     }
 
+    const nodes = [libraryNode, decendants];
+
+    const newAncestry = parentDoc.ancestors || [];
+    newAncestry.push(parent);
     // re-map all the ancestors
     setLineageOfDocs({
       docArray: nodes,
-      newAncestry: [
-        ...libraryNode.ancestors,
-        { id: libraryNodeId, collection: 'libraryNodes' }
-      ],
-      oldParent: { id: _id, collection: 'libraryNodes' },
+      newAncestry,
+      oldParent: libraryNode.parent,
     });
 
     // Give the docs new IDs without breaking internal references
     renewDocIds({ docArray: nodes });
 
     // Order the root node
-    libraryNode.order += 0.5;
+    libraryNode.order = (parentDoc.order || 0) + 0.5;
 
-    LibraryNodes.batchInsert([libraryNode, ...nodes]);
+    LibraryNodes.batchInsert(nodes);
 
     // Tree structure changed by inserts, reorder the tree
     reorderDocs({
       collection: LibraryNodes,
-      ancestorId: libraryNode.ancestors[0].id,
+      ancestorId: parent.collection === 'libraries' ? parent.id : parentDoc.ancestors[0].id,
     });
 
     return libraryNodeId;
