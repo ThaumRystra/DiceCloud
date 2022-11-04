0bfdb73b474ef2f182f4bf7240d285728ecd7399
Stefan Zermatten <stefanzermatten@gmail.com>
2022-11-04 12:34:37 +0200

    Added a quick exit for migrations if the database is new

--- app/imports/constants/MAINTENANCE_MODE.js
+++ app/imports/constants/MAINTENANCE_MODE.js
@@ -1,15 +1,23 @@
 import { Migrations } from 'meteor/percolate:migrations';
 import SCHEMA_VERSION from '/imports/constants/SCHEMA_VERSION.js';
 
-if (Meteor.isServer){
-  Meteor.startup(()=>{
+if (Meteor.isServer) {
+  Meteor.startup(() => {
     const dbVersion = Migrations.getVersion();
+    // If there are no users, this is a new DB, set the version to latest
+    const aUser = Meteor.users.findOne({});
+    const latestVersion = Migrations._list[Migrations._list.length - 1].version
+    if (!aUser && dbVersion !== latestVersion) {
+      Migrations._collection.update({ _id: 'control' }, { version: latestVersion });
+      return;
+    }
+    // Otherwise put the app in maintenance mode if it's not the right version
     if (
       !Meteor.settings.public.maintenanceMode &&
       dbVersion !== undefined &&
       SCHEMA_VERSION !== dbVersion
-    ){
-      Meteor.settings.public.maintenanceMode =  {
+    ) {
+      Meteor.settings.public.maintenanceMode = {
         reason: 'App data needs to be migrated to the latest version'
       };
     }
