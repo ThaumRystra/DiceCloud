cbbbcaf56afb7f4f601e36be306692523dd0f382
Stefan Zermatten <stefanzermatten@gmail.com>
2023-01-31 14:51:39 +0200

    Changed variable name regex to allow $ and _

--- app/imports/constants/VARIABLE_NAME_REGEX.js
+++ app/imports/constants/VARIABLE_NAME_REGEX.js
@@ -1,4 +1,4 @@
 // Must contain a letter, and be made of word characters only
-const VARIABLE_NAME_REGEX = /^[a-z][\w-]*$/i;
+const VARIABLE_NAME_REGEX = /^[a-z$][\w_]*$/i;
 
 export default VARIABLE_NAME_REGEX;
