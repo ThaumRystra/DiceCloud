16de798916795f563647338ef5e17b98689ccb59
Stefan Zermatten <stefanzermatten@gmail.com>
2023-05-01 18:30:13 +0200

    slot fill filter now looks at libraryTags not tags

--- app/imports/api/creature/creatureProperties/methods/getSlotFillFilter.js
+++ app/imports/api/creature/creatureProperties/methods/getSlotFillFilter.js
@@ -1,16 +1,18 @@
-export default function getSlotFillFilter({slot, libraryIds}){
+export default function getSlotFillFilter({ slot, libraryIds }) {
+
+  if (!slot) throw 'Slot is required';
+  if (!libraryIds) throw 'LibraryIds is required';
+
   let filter = {
-    removed: {$ne: true},
+    removed: { $ne: true },
     $and: []
   };
-  if (libraryIds){
-    filter['ancestors.id'] = {$in: libraryIds};
-  }
-  if (slot.slotType){
+  filter['ancestors.id'] = { $in: libraryIds };
+  if (slot.slotType) {
     filter.$and.push({
       $or: [{
         type: slot.slotType
-      },{
+      }, {
         type: 'slotFiller',
         slotFillerType: slot.slotType,
       }]
@@ -19,44 +21,44 @@ export default function getSlotFillFilter({slot, libraryIds}){
     filter.$and.push({
       $or: [{
         type: 'classLevel',
-      },{
+      }, {
         type: 'slotFiller',
         slotFillerType: 'classLevel',
       }]
     });
-    if (slot.variableName) { 
+    if (slot.variableName) {
       filter.variableName = slot.variableName;
     }
 
     // Only search for levels the class needs
     if (slot.missingLevels && slot.missingLevels.length) {
-      filter.level = {$in: slot.missingLevels};
+      filter.level = { $in: slot.missingLevels };
     } else {
       filter.level = (slot.level || 0) + 1;
     }
   }
   let tagsOr = [];
   let tagsNin = [];
-  if (slot.slotTags && slot.slotTags.length){
-    tagsOr.push({tags: {$all: slot.slotTags}});
+  if (slot.slotTags && slot.slotTags.length) {
+    tagsOr.push({ libraryTags: { $all: slot.slotTags } });
   }
-  if (slot.extraTags && slot.extraTags.length){
+  if (slot.extraTags && slot.extraTags.length) {
     slot.extraTags.forEach(extra => {
       if (!extra.tags || !extra.tags.length) return;
-      if (extra.operation === 'OR'){
-        tagsOr.push({tags: {$all: extra.tags}});
-      } else if (extra.operation === 'NOT'){
+      if (extra.operation === 'OR') {
+        tagsOr.push({ libraryTags: { $all: extra.tags } });
+      } else if (extra.operation === 'NOT') {
         tagsNin.push(...extra.tags);
       }
     });
   }
-  if (tagsOr.length){
+  if (tagsOr.length) {
     filter.$or = tagsOr;
   }
-  if (tagsNin.length){
-    filter.$and.push({tags: {$nin: tagsNin}});
+  if (tagsNin.length) {
+    filter.$and.push({ libraryTags: { $nin: tagsNin } });
   }
-  if (!filter.$and.length){
+  if (!filter.$and.length) {
     delete filter.$and;
   }
   return filter;
