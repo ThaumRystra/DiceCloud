c24247cf386fc46467eae8e676bfe9d40e88666e
Stefan Zermatten <stefanzermatten@gmail.com>
2023-06-14 13:57:30 +0200

    Replaced dash-minus with unicode minus in most places

--- app/imports/api/utility/numberToSignedString.js
+++ app/imports/api/utility/numberToSignedString.js
@@ -1,10 +1,11 @@
-export default function numberToSignedString(number, spaced){
+export default function numberToSignedString(number, spaced) {
   if (typeof number !== 'number') return number;
-  if (number === 0){
+  if (number === 0) {
     return spaced ? '+ 0' : '+0';
-  } else if (number > 0){
+  } else if (number > 0) {
     return spaced ? `+ ${number}` : `+${number}`;
   } else {
-    return spaced ? `- ${Math.abs(number) || number}` : `${number}`;
+    // Uses the unicode minus sign '−' instead of a dash '-' to help line up numbers nicely
+    return spaced ? `− ${Math.abs(number) || number}` : `−${Math.abs(number) || number}`;
   }
 }
