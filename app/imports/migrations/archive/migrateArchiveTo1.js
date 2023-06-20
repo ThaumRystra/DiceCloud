beb4d9467629a5d8232a75d7c029d333f1bf3444
Stefan Zermatten <stefanzermatten@gmail.com>
2023-06-20 13:30:35 +0200

    Added archive migrations to schema version 2

--- app/imports/migrations/archive/migrateArchiveTo1.js
+++ app/imports/migrations/archive/migrateArchiveTo1.js
@@ -1,39 +1,45 @@
 import CreatureProperties from '/imports/api/creature/creatureProperties/CreatureProperties.js';
 import { get, set } from 'lodash';
 import applyFnToKey from '/imports/api/engine/computation/utility/applyFnToKey.js';
-import  { calculationUp } from '/imports/migrations/server/dbv1/dbv1.js';
 
-export default function cleanAt1(archive){
+
+function calculationUp(val) {
+  if (typeof val !== 'string') return val;
+  if (!val.replace) console.log({ val, replace: val.replace });
+  return val.replace(/#(\w+).(\w+)Result/g, '#$1.$2')
+    .replace(/\.value/g, '.total')
+    .replace(/\.currentValue/g, '.value');
+}
+
+export default function migrateTo1(archive) {
   archive.properties = archive.properties.map(prop => {
-    let cleanProp = prop;
     try {
       if (prop.type === 'attack') prop.type = 'action';
+      if (prop.type === 'slotFiller') prop.type = 'folder';
       // Get the schema
       const schema = CreatureProperties.simpleSchema(prop);
       // Clean all the text fields with inline calcs
       schema.inlineCalculationFields().forEach(key => {
         applyFnToKey(prop, key, (prop, key) => {
           let field = get(prop, key);
-          if (typeof field === 'string' || typeof field === 'number'){
+          if (typeof field === 'string' || typeof field === 'number') {
             field = calculationUp(field);
-            set(prop, key, {text: `${field}`});
+            set(prop, key, { text: `${field}` });
           }
         });
       });
       schema.computedFields().forEach(key => {
         applyFnToKey(prop, key, (prop, key) => {
           let field = get(prop, key) || get(prop, key + 'Calculation');
-          if (typeof field === 'string' || typeof field === 'number'){
+          if (typeof field === 'string' || typeof field === 'number') {
             field = calculationUp(field);
-            set(prop, key, {calculation: `${field}`});
+            set(prop, key, { calculation: `${field}` });
           }
         });
       });
-      cleanProp = schema.clean(prop);
-      schema.validate(cleanProp);
-    } catch (e){
-      console.warn({propId: prop._id, error: e.message || e.reason || e.toString()});
+    } catch (e) {
+      console.warn('Property migration -> 1 failed: ', { propId: prop._id, error: e.message || e.reason || e.toString() });
     }
-    return cleanProp;
+    return prop;
   });
 }
--- app/imports/migrations/server/dbv2/cleanAt2.js
+++
@@ -1,3 +0,0 @@
-export default function cleanAt2() {
-  return;
-}
