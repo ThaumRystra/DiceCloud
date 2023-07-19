4133a0f78c0e8b2e5c6380bf920fe07991dbc041
Stefan Zermatten <stefanzermatten@gmail.com>
2023-07-19 19:40:59 +0200

    Fixed proficiency bonus not applying in actions

--- app/imports/api/engine/computation/utility/evaluateCalculation.js
+++ app/imports/api/engine/computation/utility/evaluateCalculation.js
@@ -1,14 +1,14 @@
 import resolve, { toString } from '/imports/parser/resolve.js';
 
-export default function evaluateCalculation(calculation, scope, givenContext){
+export default function evaluateCalculation(calculation, scope, givenContext) {
   const parseNode = calculation.parseNode;
   const fn = calculation._parseLevel;
-  const calculationScope = {...calculation._localScope, ...scope};
-  const {result: resultNode, context} = resolve(fn, parseNode, calculationScope, givenContext);
+  const calculationScope = { ...calculation._localScope, ...scope };
+  const { result: resultNode, context } = resolve(fn, parseNode, calculationScope, givenContext);
   calculation.errors = context.errors;
-  if (resultNode?.parseType === 'constant'){
+  if (resultNode?.parseType === 'constant') {
     calculation.value = resultNode.value;
-  } else if (resultNode?.parseType === 'error'){
+  } else if (resultNode?.parseType === 'error') {
     calculation.value = null;
   } else {
     calculation.value = toString(resultNode);
