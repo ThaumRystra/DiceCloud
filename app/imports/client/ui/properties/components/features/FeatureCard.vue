6ff4b9c3f2f0a5a9f1130f5c1681d4638065f5dd
Chris Feger <christopherfeger@yahoo.com>
2023-09-14 15:25:23 -0700

    Add icons to feature display

--- app/imports/client/ui/properties/components/features/FeatureCard.vue
+++ app/imports/client/ui/properties/components/features/FeatureCard.vue
@@ -6,6 +6,11 @@
   >
     <template slot="toolbar">
       <v-toolbar-title>
+        <property-icon v-if="model.icon"
+          class="mr-2"
+          :model="model"
+          :color="model.color"
+        />
         {{ model.name }}
       </v-toolbar-title>
       <v-spacer />
@@ -22,11 +27,13 @@
 <script lang="js">
 import ToolbarCard from '/imports/client/ui/components/ToolbarCard.vue';
 import PropertyDescription from '/imports/client/ui/properties/viewers/shared/PropertyDescription.vue'
+import PropertyIcon from '/imports/client/ui/properties/shared/PropertyIcon.vue';
 
 export default {
   components: {
     ToolbarCard,
     PropertyDescription,
+    PropertyIcon,
   },
   props: {
     model: {
