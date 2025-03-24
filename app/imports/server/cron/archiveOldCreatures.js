30dab1ed6ac027ac83592478eb616c3299483484
LinuxMaster393 <73559323+linuxmaster393@users.noreply.github.com>
2025-03-24 11:57:46 -0400

    Write cron job to process all creatures and archive any that haven't been updated since duration configured in Meteor settings.

---
+++ app/imports/server/cron/archiveOldCreatures.js
@@ -0,0 +1,44 @@
+import Creatures from '/imports/api/creature/creatures/Creatures';
+import archiveCreature from '/imports/api/creature/archive/methods/archiveCreatureToFile';
+import { assertAdmin } from '/imports/api/sharing/sharingPermissions';
+import { SyncedCron } from 'meteor/littledata:synced-cron';
+const archiveAfterDays = !!Meteor.settings?.archiveAfterDays;
+
+Meteor.startup(() => {
+  /**
+   * Archive all creatures older than the configured amount of days.
+   */
+  const archiveOldCreatures = function () {
+    if (typeof archiveAfterDays != 'number') {
+      return;
+    }
+    const now = new Date();
+    const expire = new Date(now.getTime() - (archiveAfterDays * 24 * 60 * 60 * 1000));
+    Creatures.find({ lastComputeTime: { $lt: expire }}, { _id: 1 }).forEach( creature => {
+      archiveCreature(creature._id, function (error) {
+        if (error) {
+          console.error(JSON.stringify(error, null, 2));
+        }
+      });
+    });
+  }
+
+  SyncedCron.add({
+    name: 'archiveOldCreatures',
+    schedule: function (parser) {
+      return parser.text('every 1 hour');
+    },
+    job: archiveOldCreatures,
+  });
+
+  SyncedCron.start();
+
+  // Add a method to manually trigger removal
+  Meteor.methods({
+    archiveOldCreatures() {
+      assertAdmin(this.userId);
+      this.unblock();
+      archiveOldCreatures();
+    },
+  });
+});
