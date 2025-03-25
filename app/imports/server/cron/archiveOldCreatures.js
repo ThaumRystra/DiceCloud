aa020d928c0bb11136184163c3bb699e9fd7fa86
LinuxMaster393 <73559323+linuxmaster393@users.noreply.github.com>
2025-03-25 14:16:11 -0400

    Fix key value for lastComputedAt in the auto archive cron job.

--- app/imports/server/cron/archiveOldCreatures.js
+++ app/imports/server/cron/archiveOldCreatures.js
@@ -16,7 +16,7 @@ Meteor.startup(() => {
     const now = new Date();
     const expire = new Date(now.getTime() - (archiveAfterDays * 24 * 60 * 60 * 1000));
     console.log("archiving all creatures after " + expire);
-    Creatures.find({ lastComputeTime: { $lt: expire }}, { _id: 1 }).forEach( creature => {
+    Creatures.find({ lastComputedAt: { $lt: expire }}, { _id: 1 }).forEach( creature => {
       console.log("archiving " + creature._id);
       archiveCreature(creature._id, function (error) {
         console.log("Archive callback")
